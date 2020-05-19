804a57a86af916e6bb88024aea3b3278bf5e94b4
Seeingu <seeingasu@gmail.com>
2020-05-19 20:02:37 +0800

    init main page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./src/**/*.tsx', './pages/**/*.tsx'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        default: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
