a6e81b3407a9d1784a103c107c9b6226050f8950
Seeingu <seeingasu@gmail.com>
2020-07-05 14:30:19 +0800

    chore: rename company

--- pages/about.tsx
+++ pages/about.tsx
@@ -19,7 +19,7 @@ const About = () => {
           </li>
           <li>
             <FontAwesomeIcon fixedWidth icon={faBuilding} size="lg" /> Shanghai
-            Bytedance inc.
+            ByteDance inc.
           </li>
           <li>
             <FontAwesomeIcon fixedWidth icon={faPaperPlane} size="lg" />{' '}
